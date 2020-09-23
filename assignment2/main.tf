b74f35084bd8fd541c8c72a725316b0b88c3667e
mukeshnegi2108 <37812767+mukeshnegi2108@users.noreply.github.com>
2020-09-23 12:39:40 +0530

    Create main.tf

---
+++ assignment2/main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+variable "prefix" {
+  default = "maersk"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = "West US 2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  tags = {
+    environment = "development"
+  }
+}
